da1dec471c012e890d46a2df9543c33ec1c6f1b6
eugene <eugene.shiyanov@gmail.com>
2013-03-03 22:19:08 +0200

    create user tables

--- support/create_db.sql
+++ support/create_db.sql
@@ -1,19 +1,49 @@
 -- clear db
-
-drop table if exists Items;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS stores;
+DROP TABLE IF EXISTS usergroups;
+DROP TABLE IF EXISTS users;
 
 -- create tables
-create table if not exists Items (
-	item_id bigint auto_increment primary key,
-	name char(255) not null,
-	price double
+CREATE TABLE IF NOT EXISTS items (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	name CHAR(255) NOT NULL,
+	price DOUBLE
+);
+
+CREATE TABLE IF NOT EXISTS stores (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name CHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS usergroups (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name CHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    login CHAR(255) NOT NULL UNIQUE,
+    email CHAR(255) NOT NULL UNIQUE,
+    first_name CHAR(255) NOT NULL,
+    last_name CHAR(255) NOT NULL,
+    patronymic_name CHAR(255) NOT NULL,
+    usergroup_id INT NOT NULL,
+    password CHAR(255) NOT NULL,
+    CONSTRAINT usergroups_id_fk
+    FOREIGN KEY (usergroup_id)
+    REFERENCES usergroups (id)
 );
 
 -- insert data
-insert into Items(name, price) values('CPU', 50.5);
-insert into Items(name, price) values('VGA', 60.6);
-insert into Items(name, price) values('HDD', 70.7);
+INSERT INTO items(name, price) VALUES('CPU', 50.5);
+INSERT INTO items(name, price) VALUES('VGA', 60.6);
+INSERT INTO items(name, price) VALUES('HDD', 70.7);
 
--- create the same for Stores
-create table if not exists Stores (stores_id bigint auto_increment primary key, name char(255) not null);
-insert into Stores (name) values ('Podval');
+INSERT INTO stores (name) VALUES ('Podval');
+
+INSERT INTO usergroups(name) VALUES('ADMINISTRATORS');
+INSERT INTO usergroups(name) VALUES('USERS');
+
+INSERT INTO users(login, email, first_name, last_name, patronymic_name, usergroup_id, password)
+VALUES('admin', '', '', '', '', 1, 'yjc');
